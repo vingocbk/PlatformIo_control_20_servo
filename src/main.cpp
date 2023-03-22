a019e6828a4020e52f0a39d34f2bfc9fa89eb487
vingocbk <vi.ngoc.bk@gmail.com>
2023-03-22 16:51:26 +0700

    update company to home 22_3

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,13 @@ PCA9685 pwmController_1(ADDRESS_PCA9685_1);           // Library using Wire1 @40
 PCA9685 pwmController_2(ADDRESS_PCA9685_2);           // Library using Wire1 @400kHz
 PCA9685_ServoEval pwmServo;
 
-
+String dataSendPreset = "[{\"1\":\"ngoc\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"tuyet\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"tuan\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"hcchu\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"hxhy\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"gxyxy\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"hxhx\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"t8t7ry\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{\"1\":\"hxhxucupc\",\"2\":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}]#";
+String dataSendTour = "{\"1\":[{\"1\":\"ngoc\",\"2\":50},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128}],\"2\":[{\"1\":\"tuyet\",\"2\":40},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128}],\"3\":[{\"1\":\"tuan\",\"2\":50},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128}],\"4\":[{\"1\":\"tuan\",\"2\":5},{\"1\":\"ngoc\",\"2\":128},{\"1\":\"ngoc\",\"2\":128}],\"5\":[3,1]}%";
 void pca9685Init();
 void bluetoothInit();
 void callbackBluetooth(esp_spp_cb_event_t event, esp_spp_cb_param_t *param);
-void ReadPulseInMode3(void *pvParameters);
+void readPulseInMode3(void *pvParameters);
+void sendDataToApp(String data);
 
 
 void pca9685Init(){
@@ -50,7 +52,6 @@ void callbackBluetooth(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
   	{
   	case ESP_SPP_SRV_OPEN_EVT:
     	ECHOLN("Client Connected");
-        APP_FLAG_SET(SEND_CURRENT_MIN_MAX);
     	break;
     case ESP_SPP_CLOSE_EVT:
         ECHOLN("Client Disconnected");
@@ -69,7 +70,43 @@ void callbackBluetooth(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
             JsonObject& rootData = jsonBuffer.parseObject(data);
             if(rootData.success())
             {
+                String type = rootData["type"];
+                if(type == "sync_preset"){
+                    sendDataToApp(dataSendPreset);
+                }
+                else if(type == "sync_tour"){
+                    sendDataToApp(dataSendTour);
+                }
+                else if(type == "save_preset"){
+                    
+                }
+                else if(type == "save_tour"){
 
+                }
+                else if(type == "control"){
+                    String mode = rootData["mode"];
+                    String data = rootData["data"];
+                    if(mode == "preset"){
+                        
+                    }
+                    else if(mode == "tour"){
+                        if(data == "mode 1"){
+
+                        }
+                        else if(data == "mode 2"){
+
+                        }
+                        else if(data == "mode 3"){
+
+                        }
+                        else if(data == "mode 4"){
+
+                        }
+                        else if(data == "mode 5"){
+
+                        }
+                    }
+                }
             }
         }
         break;
@@ -79,8 +116,7 @@ void callbackBluetooth(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
 }
 
 
-
-void ReadPulseInMode3(void *pvParameters){
+void readPulseInMode3(void *pvParameters){
     bool check_start_connect_mode_run = false;
     unsigned long check_time_begin;
 	for( ;; )
@@ -89,7 +125,12 @@ void ReadPulseInMode3(void *pvParameters){
 	}
 }
 
-
+void sendDataToApp(String data){
+    ECHOLN("sendDataToApp");
+    for(int i = 0; i < data.length(); i++){
+        SerialBT.write(data[i]);
+    }
+}
 
 
 void setup() {
@@ -98,13 +139,14 @@ void setup() {
     Serial.begin (SERIAL_BAUDRATE);  
     Wire.begin (SDA_PIN, SCL_PIN);   // sda= GPIO_21 /scl= GPIO_22
     EEPROM.begin(MAX_SIZE_EEPROM_BUFFER);
-	pca9685Init();
+	// pca9685Init();
 	bluetoothInit();
+    pinMode(0, INPUT_PULLUP);
 
 
 	xTaskCreatePinnedToCore(
-		ReadPulseInMode3,    /* Function to implement the task */
-		"ReadPulseInMode3",  /* Name of the task */
+		readPulseInMode3,    /* Function to implement the task */
+		"readPulseInMode3",  /* Name of the task */
 		4096,             /* Stack size in words */
 		NULL,             /* Task input parameter */
 		1,                /* Priority of the task */
@@ -114,4 +156,8 @@ void setup() {
 
 void loop() {
   	// put your main code here, to run repeatedly:
+    if(digitalRead(0) == LOW){
+        vTaskDelay(500/portTICK_RATE_MS);
+        sendDataToApp(dataSendPreset);
+    }
 }
